1abda36bbcc0dfbf1ae16665fb607a0e39b57356
tdiepenb <tdiepenb@smail.uni-koeln.de>
2022-12-03 20:53:13 +0100

    some commit

--- src/de/uni/koeln/sse/se/Main.java
+++ src/de/uni/koeln/sse/se/Main.java
@@ -1,2 +1,35 @@
-package de.uni.koeln.sse.se;public class Main {
+package de.uni.koeln.sse.se;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //debuggingExercise1_2();
+
+        debuggingExercise3();
+
+    }
+
+
+    public static void debuggingExercise1_2() {
+
+        ShoppingBasket johnShoppingBasket = new ShoppingBasket();
+        johnShoppingBasket.addTobasket("Bread");
+        johnShoppingBasket.addTobasket("Water");
+        System.out.println("john bill : " + johnShoppingBasket.getBill());
+
+        ShoppingBasket saraShoppingBasket = new ShoppingBasket();
+        saraShoppingBasket.addTobasket("Pasta");
+        saraShoppingBasket.addTobasket("Milk");
+        System.out.println("sara bill : " + saraShoppingBasket.getBill());
+
+    }
+
+
+    public static void debuggingExercise3() {
+        MonthPoem myMonth = new MonthPoem(Month.MAY);
+        myMonth.PrintPoem();
+
+    }
+
 }
--- src/de/uni/koeln/sse/se/ShoppingBasket.java
+++ src/de/uni/koeln/sse/se/ShoppingBasket.java
@@ -3,26 +3,37 @@ package de.uni.koeln.sse.se;
 import java.util.ArrayList;
 
 public class ShoppingBasket {
-    public static Items item_list = new Items();
-    private static Integer totalPrice = 0;
-    ArrayList<String> MyItems = new ArrayList();
+    public static Items item_list=new Items();
+    private Integer totalPrice=0;
+
+
+    ArrayList<String> MyItems=new ArrayList<String>();
 
     public ShoppingBasket() {
+
     }
 
     public void addTobasket(String item) {
-        this.setTotalPrice(this.getTotalPrice() + item_list.getPrice(item));
+
+
+        setTotalPrice(getTotalPrice()+ item_list.getPrice(item));
+
+
     }
 
     public Integer getBill() {
-        return this.getTotalPrice();
+
+        return getTotalPrice();
+
+
     }
 
-    public Integer getTotalPrice() {
+    public  Integer getTotalPrice() {
         return totalPrice;
     }
 
-    public void setTotalPrice(Integer totalPrice) {
-        ShoppingBasket.totalPrice = totalPrice;
+    public  void setTotalPrice(Integer totalPrice) {
+        this.totalPrice = totalPrice;
     }
+
 }
